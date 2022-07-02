24899bb82cfb55feb5a5b5aa96958ebe0140b73e
Yehia-Ahmad <yehia790200@gmail.com>
2022-07-02 23:09:59 +0200

    <msg>

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem () {
+    return <div className= "expense-item">
+            <div>March 28th 2022</div>
+            <div className = "expense-item__discription">
+                <h2>Car Insurence</h2>
+                <div className = "expense-item__price">$248.61</div>
+            </div>
+           </div>
+}
+
+export default ExpenseItem;
